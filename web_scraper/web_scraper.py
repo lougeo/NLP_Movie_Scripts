a441211cb142f3a11bc59c94f423d8f35fe463da
Louis George <louisg495@gmail.com>
2020-03-01 10:04:42 -0800

    Link/title getting function fully operational

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -7,6 +7,10 @@ from time import sleep
 
 import requests
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 class ScriptScraper():
@@ -14,29 +18,39 @@ class ScriptScraper():
     def __init__(self):
         self.driver = webdriver.Chrome(r"C:\Users\louis\Downloads\Chromedriver\chromedriver_win32\chromedriver.exe")
 
-    def getit(self):
+    def num_elements(self):
 
         self.movie_links = []
         self.movie_titles = []
-        self.genre = []
-        self.movie_script = []
 
         # Loads the page listing all movies
         self.driver.get("https://www.imsdb.com/all%20scripts/")
 
+        # the wait code was copied from a kind soul on stack overflow @:
+        # https://stackoverflow.com/questions/26566799/wait-until-page-is-loaded-with-selenium-webdriver-for-python 
+        #####
+        timeout = 10
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '/html/body/table[2]/tbody/tr/td[3]/p'))
+            WebDriverWait(self.driver, timeout).until(element_present)
+        except TimeoutException:
+            print("Timed out waiting for page to load")
+        #####
+        print("I'm here")
         # Find and get links to movies
         for i in self.driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td[3]/p"):
-            movie_links.append(i.find_element_by_xpath('.//a').get_attribute('href'))
+            self.movie_links.append(i.find_element_by_xpath('.//a').get_attribute('href'))
+            self.movie_titles.append(i.find_element_by_xpath('.//a').get_attribute('text'))
             
-            first_button = i.find_element_by_xpath('.//a')
-            first_button.click()
+    def gitit(self):
 
-            movie_title = self.driver.find_element_by_xpath('//*[@id="mainbody"]/table[2]/tbody/tr/td[3]/table[1]/tbody/tr[1]/td/h1')
+        for i in self.movie_links:
+            pass
 
-            if link_count > 1:
-                break
+
 
     def no_loop_test(self):
+
         test_movie_titles = []
         test_genre = []
         test_movie_script = []
