0593f6360e778df1be9b168b986c1549623626d0
Louis George <louisg495@gmail.com>
2020-02-29 22:09:03 -0800

    Unlooped function fully operational.

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -11,19 +11,21 @@ from bs4 import BeautifulSoup
 
 class ScriptScraper():
 
-    movie_links = []
-    
-    link_count = 0
-
     def __init__(self):
         self.driver = webdriver.Chrome(r"C:\Users\louis\Downloads\Chromedriver\chromedriver_win32\chromedriver.exe")
 
-    def num_elements(self):
+    def getit(self):
+
+        self.movie_links = []
+        self.movie_titles = []
+        self.genre = []
+        self.movie_script = []
+
         # Loads the page listing all movies
         self.driver.get("https://www.imsdb.com/all%20scripts/")
+
         # Find and get links to movies
         for i in self.driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td[3]/p"):
-            link_count += 1
             movie_links.append(i.find_element_by_xpath('.//a').get_attribute('href'))
             
             first_button = i.find_element_by_xpath('.//a')
@@ -35,26 +37,32 @@ class ScriptScraper():
                 break
 
     def no_loop_test(self):
-        movie_titles = []
-        movie_script = []
+        test_movie_titles = []
+        test_genre = []
+        test_movie_script = []
         # Loads the page listing all movies
         self.driver.get("https://www.imsdb.com/all%20scripts/")
 
         sleep(2)
-        
-        movie_titles.append(self.driver.find_element_by_xpath('//*[@id="mainbody"]/table[2]/tbody/tr/td[3]/p[1]/a').get_attribute('title'))
-
+        # gets movie title
+        test_movie_titles.append(self.driver.find_element_by_xpath('//*[@id="mainbody"]/table[2]/tbody/tr/td[3]/p[1]/a').get_attribute('text'))
+        # moves to intermediate page
         first_button = self.driver.find_element_by_xpath("/html/body/table[2]/tbody/tr/td[3]/p[1]/a")
         first_button.click()
         
         sleep(2)
-
+        # moves to script page
         script_button = self.driver.find_element_by_xpath('//*[@id="mainbody"]/table[2]/tbody/tr/td[3]/table[1]/tbody/tr[2]/td[2]/a[6]')
         script_button.click()
 
         sleep(2)
+        # Gets script
+        test_movie_script.append(self.driver.find_element_by_xpath('//*[@id="mainbody"]/table[2]/tbody/tr/td[3]/table/tbody/tr/td/pre/pre').text)
+        # Gets genre
+        genre_inter = []
+        for i in self.driver.find_elements_by_xpath('//*[@id="mainbody"]/table[2]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr/td[2]/a[contains(@href, "/genre")]'):
+            genre_inter.append(i.get_attribute('text'))
+        test_genre.append(genre_inter)
 
-        movie_script.append(self.driver.find_element_by_xpath('//*[@id="mainbody"]/table[2]/tbody/tr/td[3]/table/tbody/tr/td/pre/pre').text)
-
-        return movie_titles, len(movie_script)
+        return test_movie_titles, len(test_movie_script), test_genre
 
