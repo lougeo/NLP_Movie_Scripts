5083c2a5da02bed41cab393d294bcac90778cea6
Louis George <louisg495@gmail.com>
2020-03-02 21:32:20 -0800

    Batched the query

--- tomato_scraper.py
+++ tomato_scraper.py
@@ -4,7 +4,6 @@
 
 import numpy as np
 import pandas as pd 
-from time import sleep
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
@@ -18,15 +17,18 @@ class ScoreScraper():
     def __init__(self):
         self.driver = webdriver.Chrome(r"C:\Users\louis\Downloads\Chromedriver\chromedriver_win32\chromedriver.exe")
 
-    def scores(self):
+    def tables(self):
 
-        movie_info = []
         # Split into two batches as only 1000 requests aloud per day
         titles = pd.read_csv('../movie_titles.csv').drop('Unnamed: 0', axis=1)
-        titles1 = titles.iloc[:500, :]
-        titles2 = titles.iloc[500:, :]
+        self.titles1 = titles.iloc[:500, :]
+        self.titles2 = titles.iloc[500:, :]
 
-        for i in titles['titles']:
+    def getit1(self):
+
+        movie_info = []
+
+        for i in self.titles1['titles']:
             try:
                 self.driver.get(f'http://www.omdbapi.com/?apikey=fd40c3c8&t={i}')
 
@@ -43,4 +45,35 @@ class ScoreScraper():
             except:
                 # Appends nan if timeout
                 movie_info.append(np.nan)
+
+            df_movie_info1 = pd.DataFrame({'titles':self.titles1,
+                                        'info':movie_info})
+            df_movie_info1.to_csv('movie_info1.csv')
+    
+    def getit2(self):
+
+        movie_info = []
+
+        for i in self.titles1['titles']:
+            try:
+                self.driver.get(f'http://www.omdbapi.com/?apikey=fd40c3c8&t={i}')
+
+                # Wait
+                try:
+                    element_present = EC.presence_of_element_located((By.XPATH, '/html/body/pre'))
+                    WebDriverWait(self.driver, timeout).until(element_present)
+                except TimeoutException:
+                    print("Timed out waiting for page to load")
+                
+                # Appending full json to list
+                movie_info.append(self.driver.find_element_by_xpath('/html/body/pre').text)
+            
+            except:
+                # Appends nan if timeout
+                movie_info.append(np.nan)
+
+                
+            df_movie_info2 = pd.DataFrame({'titles':self.titles2,
+                                        'info':movie_info})
+            df_movie_info2.to_csv('movie_info2.csv')
     
